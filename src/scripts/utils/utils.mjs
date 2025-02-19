3d6ac774b284df74e157f3dfa46c10e725420c90
Cristobal Henriquez <brohenriquez@gmail.com>
2025-02-19 00:06:20 -0500

    changed file name

---
+++ src/scripts/utils/utils.mjs
@@ -0,0 +1,38 @@
+//converts a fetch response to JSON
+export function convertToJson(res) {
+    if (res.ok) {
+        return res.json();
+    } else {
+        throw new Error("Bad Response");
+    }
+}
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+export function renderListWithTemplate(
+    templateFn,
+    parentElement,
+    list,
+    position = "afterbegin",
+    clear = false,
+  ) {
+    const htmlStrings = list.map(templateFn);
+    // if clear is true we need to clear out the contents of the parent.
+    if (clear) {
+      parentElement.innerHTML = "";
+    }
+    parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+  }
+  
+// wrapper for querySelector...returns matching element
+export function qs(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
