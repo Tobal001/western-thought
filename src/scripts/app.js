6e8b16479691d1627da0f043a866b04aef788f95
Cristobal Henriquez <brohenriquez@gmail.com>
2025-02-21 21:53:22 -0500

    fixed bug

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,7 +5,7 @@ import PhilosophyEras from "./ui/uiTimeline.mjs";
 
 const dataService = new philosophyData("western");
 const timelineContainer = document.getElementById("timeline-container")
-const eras = dataService.getData();
+const eras = await dataService.getData();
 
 const timeline = new PhilosophyEras(eras, timelineContainer, dataService)
 timeline.init();
