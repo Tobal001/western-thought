294e35e6e3d308068aa699e13fb7add729f9d9b8
Cristobal Henriquez <brohenriquez@gmail.com>
2025-02-21 22:26:25 -0500

    async fix

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,9 +5,10 @@ import PhilosophyEras from "./ui/uiTimeline.mjs";
 
 const dataService = new philosophyData("western");
 const timelineContainer = document.getElementById("timeline-container")
+const eras = await dataService.getData();
 
 (async function initApp() {
     const eras = await dataService.getData();
     const timeline = new PhilosophyEras(eras, timelineContainer, dataService);
     timeline.init();
-  })();
+})();
